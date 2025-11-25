3bd28d8961fd18e8a24e3be63f81f1b11f919364
Siggi <github@icellan.net>
2025-11-25 13:01:22 +0100

    Set the block height in setTxMined test to actually expected height

--- services/blockvalidation/BlockValidation_test.go
+++ services/blockvalidation/BlockValidation_test.go
@@ -1823,7 +1823,7 @@ func TestBlockValidation_DoubleSpendInBlock(t *testing.T) {
 		blockHeader,
 		coinbaseTx,
 		subtreeHashes,
-		uint64(subtree.Length()), //nolint:gosec
+		uint64(subtree.Length()),                        //nolint:gosec
 		uint64(coinbaseTx.Size()+tx1.Size()+tx2.Size()), //nolint:gosec
 		100, 0,
 	)
@@ -1938,7 +1938,7 @@ func TestBlockValidation_InvalidTransactionChainOrdering(t *testing.T) {
 		blockHeader,
 		coinbaseTx,
 		subtreeHashes,
-		uint64(subtree.Length()), //nolint:gosec
+		uint64(subtree.Length()),                        //nolint:gosec
 		uint64(coinbaseTx.Size()+tx1.Size()+tx2.Size()), //nolint:gosec
 		100, 0,
 	)
@@ -2413,7 +2413,7 @@ func TestBlockValidation_ParentAndChildInSameBlock(t *testing.T) {
 		blockHeader,
 		coinbaseTx,
 		subtreeHashes,
-		uint64(subtree.Length()), //nolint:gosec
+		uint64(subtree.Length()),                                  //nolint:gosec
 		uint64(coinbaseTx.Size()+parentTx.Size()+childTx1.Size()), //nolint:gosec
 		100, 0,
 	)
@@ -2934,7 +2934,7 @@ func setupRevalidateBlockTest(t *testing.T) (*BlockValidation, *model.Block, *bl
 		blockHeader,
 		coinbaseTx,
 		subtreeHashes,
-		uint64(subtree.Length()), //nolint:gosec
+		uint64(subtree.Length()),                                  //nolint:gosec
 		uint64(coinbaseTx.Size()+parentTx.Size()+childTx1.Size()), //nolint:gosec
 		100, 0,
 	)
@@ -3330,7 +3330,7 @@ func TestBlockValidation_SetMined_UpdatesTxMeta(t *testing.T) {
 		subtreeHashes,
 		uint64(subtree.Length()), //nolint:gosec
 		uint64(totalSize),        //nolint:gosec
-		100, 0,
+		1, 1,
 	)
 	require.NoError(t, err)
 
@@ -3354,7 +3354,7 @@ func TestBlockValidation_SetMined_UpdatesTxMeta(t *testing.T) {
 	require.NotEmpty(t, metaChildTx.BlockHeights)
 
 	require.Equal(t, metaChildTx.BlockIDs[0], uint32(0x1))
-	require.Equal(t, metaChildTx.BlockHeights[0], uint32(0x64))
+	require.Equal(t, metaChildTx.BlockHeights[0], uint32(0x1))
 	require.Equal(t, metaChildTx.SubtreeIdxs[0], 0)
 }
 
